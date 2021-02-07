9348383bc087238e4fedab8e8c7ab699f10f4155
zhenguo <zhenguo@glowing.com>
2021-02-07 09:55:44 +0800

    sync code

--- Core.lua
+++ Core.lua
@@ -38,15 +38,6 @@ RaidManager.slashOptions = {
                 RaidManager:Print(addonName .. '启用中')
             end
         },
-        ntask = {
-            name = 'ntask',
-            desc = 'create new task',
-            type = 'execute',
-            func = function ()
-                RaidManager:createTask()
-            end
-
-        },
         salary = {
             name = 'salary',
             desc = '工资邮寄管理',
@@ -79,6 +70,12 @@ function RaidManager:OnInitialize()
     RaidManager:Print(addonName .. '初始化...')
     LibStub("AceConfig-3.0"):RegisterOptionsTable(addonName, RaidManager.slashOptions, {"raidm"})
     RaidManager:Print(addonName .. '初始化成功！')
+    self.db = LibStub("AceDB-3.0"):New("RaidManagerDB")
+    local salaries = RaidManager.db.global.salaries
+    if not salaries then
+        RaidManager.db.global.salaries = {}
+    end
+    RaidManager:Print('测试global配置：' .. (RaidManager.db.global.test or ''))
 end
 
 function RaidManager:OnEnable(args)
@@ -89,47 +86,8 @@ function RaidManager:OnDisable(args)
     RaidManager:Print(addonName .. '已禁用')
 end
 
-function RaidManager:createTask ()
-    local taskTitle = nil
-    local frame = AceGUI:Create("Frame")
-    frame:SetTitle('团队管理')
-    frame:SetStatusText('启用中')
-    frame:SetCallback('OnClose', function (widget) AceGUI:Release(widget) end)
-    frame:SetLayout('Flow')
-
-    local editbox = AceGUI:Create('EditBox')
-    editbox:SetLabel("任务名称")
-    editbox:SetWidth(200)
-    editbox:SetCallback('OnEnterPressed', function(widget, event, text) taskTitle = text end)
-    frame:AddChild(editbox)
-
-    local button = AceGUI:Create("Button")
-    button:SetText('保存')
-    button:SetWidth(80)
-    button:SetCallback('OnClick', function(widget, event) RaidManager:Print('任务保存成功！' .. taskTitle) end)
-    frame:AddChild(button)
-end
-
 function RaidManager:ShowSetup ()
 end
-
-function RaidManager:DrawMemberGroups ()
-    -- 绘制团队配置,根据T、治疗、远程DPS、近战dps分组划开。
-end
-
-function RaidManager:DrawTaskManager()
-    -- 绘制任务管理与分配
-    -- 任务：坦克拉怪的标记、LR凝神的顺序、FS羊的标记。另外有一些特殊任务，比如某些特定boss、小怪的任务分配、看MT的治疗分配。
-end
-
-
-function RaidManager:AutoGroup()
-    -- 自动分组
-    -- 设置多种分组方案：1. 初始分组，4个MT在1、2组，优先QS、XD看T的血， 每组尽量一个ss，如果没有就小D，如果没有就空着， 近战组zs、lr + 其它物理攻击 + 一个ms/xd/qs；
-    --                 2. bwl老一分组，分4个组
-    --                 3. 分G的时候，根据T、治疗、DPS分组
-end
-
 
 MemberInfo = {}
 
@@ -179,26 +137,6 @@ function RaidManager:DisplayMembers()
     end
 end
 
-
-function RaidManager:AutoGroupByDefault()
-    local members = RaidManager.members
-    local classGroups = {
-    }
-    for i=1, #members do
-        local member = members[i]
-        if member.role == RaidRoles.MAINTANK then -- 主坦克 当作一个单独的职业
-            classArray = setDefault(classGroups, RaidRoles.MAINTANK, {})
-        else
-            classArray = setDefault(classGroups, member.classCode, {})
-        end
-        classArray[#classArray+1] = member;
-    end
-
-    local raidGroups = {} -- 团队分组，从1-8
-    local mainTanks = classGroups[RaidRoles.MAINTANK]
-    for i=1, #mainTanks do
-    end
-end
 
 -- utils
 local function setDefault(obj, key, defaultVal)
@@ -333,16 +271,46 @@ function RaidManager:ShowSalaryManager()
     scroll:SetLayout('List')
     scrollContainer:AddChild(scroll)
 
-    -- email content
+    -- salaries select
+    local salaries = RaidManager.db.global.salaries
+    local selectContainer = AceGUI:Create('InlineGroup')
+    selectContainer:SetLayout("Flow")
+    local dropdown = AceGUI:Create("Dropdown")
+    dropdown:SetLabel('选择工资记录')
+    dropdown:SetText("<空>")
+    dropdown:SetList({[1]="张三",[2]="李四",[3]="王五"})
+    dropdown:SetWidth(15*SCALE_LENGTH)
+    dropdown:SetHeight(SCALE_LENGTH)
+    local newSalaryEditor = RaidManager:renderEditField({
+        label = '新建',
+        width = 20 * SCALE_LENGTH,
+        OnEnterPressed = function(widget, event, text)
+            local salaries = RaidManager.db.global.salaries
+            local newSalary = {
+                title = text
+            }
+            salaries[#salaries + 1] = newSalary
+            RaidManager:Print('保存成功: ' .. RaidManager.db.global.test)
+        end
+    })
+    selectContainer:AddChild(newSalaryEditor)
+    selectContainer:AddChild(dropdown)
+    scroll:AddChild(selectContainer)
+
+    -- content
     local emailContainer = AceGUI:Create("InlineGroup")
     emailContainer:SetLayout('List')
     emailContainer:SetTitle('邮件')
     emailContainer:SetFullWidth(true)
-    emailContainer:SetHeight(20*SCALE_LENGTH)
+    emailContainer:SetHeight(20 * SCALE_LENGTH)
     emailContainer.noAutoHeight = true
     local subjectField = RaidManager:renderEditField({
         label = '标题',
         width = 400,
+        OnEnterPressed = function(widget, event, text)
+            RaidManager.db.global.test = text
+            RaidManager:Print('保存成功: ' .. RaidManager.db.global.test)
+        end
     })
     local bodyField = RaidManager:renderTextareField({
         label = '内容',
@@ -352,8 +320,6 @@ function RaidManager:ShowSalaryManager()
     emailContainer:AddChild(subjectField)
     emailContainer:AddChild(bodyField)
     scroll:AddChild(emailContainer)
-
-    scroll:AddChild(RaidManager:renderSubsidy())
 
     -- members content
     local membersGroup = RaidManager:renderMembers()
@@ -419,7 +385,9 @@ function RaidManager:renderEditField(meta)
     if meta.height then
         editbox:SetHeight(meta.height)
     end
-    editbox:SetCallback('OnEnterPressed', function(widget, event, text) val = text end)
+    if meta.OnEnterPressed then
+        editbox:SetCallback('OnEnterPressed', meta.OnEnterPressed)
+    end
 
     return editbox
 end
@@ -443,19 +411,6 @@ function RaidManager:renderTextareField(meta)
     return editbox
 end
 
-
-function RaidManager:renderSubsidy()
-    local subSidyContainer = AceGUI:Create("InlineGroup")
-    subSidyContainer:SetTitle('补贴')
-    subSidyContainer:SetFullWidth(true)
-    subSidyContainer:SetLayout('Flow')
-
-    subSidyContainer:AddChild(RaidManager:renderTankSubsidy())
-    subSidyContainer:AddChild(RaidManager:renderHealerSubsidy())
-    subSidyContainer:AddChild(RaidManager:renderDPSSubsidy())
-
-    return subSidyContainer
-end
 
 function RaidManager:renderTankSubsidy()
     local tankContainer = AceGUI:Create('InlineGroup')
